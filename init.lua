8c483e9898c43001aa346dce917025ae17a7351b
Erik Haake <ehaake@pdx.edu>
2024-03-01 15:32:24 -0800

    Add neotree

--- init.lua
+++ init.lua
@@ -21,11 +21,20 @@ vim.opt.rtp:prepend(lazypath)
 -- List of plugins to install with lazy
 local plugins = {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-     {
+	{
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
       dependencies = { 'nvim-lua/plenary.nvim' }
-    },
-    {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	},
+	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"},
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", 
+			"MunifTanjim/nui.nvim",
+		}
+	}
 }
 
 local opts = {}
