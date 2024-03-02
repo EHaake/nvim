d9583ab4e9d43e455a9d0ea38433c542db51fb69
Erik Haake <ehaake@pdx.edu>
2024-03-02 13:52:52 -0800

    Add dap dapui for debugging

---
+++ lua/plugins/debugging.lua
@@ -0,0 +1,31 @@
+return {
+	"mfussenegger/nvim-dap",
+	dependencies = {
+		"rcarriga/nvim-dap-ui",
+		"leoluz/nvim-dap-go",
+	},
+
+	config = function()
+		local dap, dapui = require("dap"), require("dapui")
+
+		require("dapui").setup()
+		require("dap-go").setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		-- keymaps
+		vim.keymap.set("n", "<leader>db", dap.toggle_breakpoint, {}) -- debugger toggle (breakpoint)
+		vim.keymap.set("n", "<leader>dc", dap.continue, {})        -- debugger continue
+	end,
+}
--- lua/vim-settings.lua
+++ lua/vim-settings.lua
@@ -1,8 +1,16 @@
+-- vim-settings.lua
+-- This file contains all the basic global vim settings I want
+
+-- tabs and spaces
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
---
-vim.opt.scrolloff = 8  -- always have at least 8 line between cursor and end of buffer
---
--- add line numbers
+
+-- Scrolling
+vim.opt.scrolloff = 8 -- always have at least 8 line between cursor and end of buffer
+
+-- Add line numbers
 vim.wo.number = true
+
+-- signcolumn always open so errors/warnings don't push buffer
+vim.o.signcolumn = "yes:1"
