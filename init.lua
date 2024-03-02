6ef7fd0ce9a2c3b6784c2b571f4ee5de7b42e68f
Erik Haake <ehaake@pdx.edu>
2024-03-01 19:17:35 -0800

    Add Mason, lsps and redefine telescope keymap

--- init.lua
+++ init.lua
@@ -3,12 +3,12 @@
 --
 -- get lazy.nvim loaded
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({ "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath, }) end
+	vim.fn.system({ "git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath, }) end
 vim.opt.rtp:prepend(lazypath)
 
 -- load external modules
