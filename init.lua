e4a7235ceada17726ada68149d2cd53a2f00ffc2
Erik Haake <ehaake@pdx.edu>
2024-03-03 14:19:51 -0800

    Add more global settings

--- init.lua
+++ init.lua
@@ -2,23 +2,11 @@
 -- This is the starting point for all configuration in Neovim
 --
 --
--- get lazy.nvim loaded
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-
-vim.opt.rtp:prepend(lazypath)
-
 -- load external modules
+require("lazy-bootstrap") -- load lazy bootstrap
+
 require("keymaps") -- load our keymaps file
+
 require("settings") -- load our settings file
+
 require("lazy").setup("plugins") -- loads everything in /plugins
--- lua/settings.lua
+++ lua/settings.lua
@@ -10,10 +10,39 @@ vim.cmd("set shiftwidth=2")
 vim.opt.scrolloff = 8 -- always have at least 8 line between cursor and end of buffer
 
 -- Add line numbers
-vim.wo.number = true
+vim.opt.number = true
 
 -- signcolumn always open so errors/warnings don't push buffer
 vim.o.signcolumn = "yes:1"
 --
 -- Set highlight on search
 vim.opt.hlsearch = true
+
+-- Don't show the mode, since it's already in status line
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Enable break indent
+vim.opt.breakindent = true
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
