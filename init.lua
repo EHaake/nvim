89b14c0f1c7c15fe432553c135b32654aefb7947
Erik Haake <ehaake@pdx.edu>
2024-03-10 22:31:18 -0700

    Reformat init.lua for concision

--- init.lua
+++ init.lua
@@ -3,11 +3,7 @@
 --
 -- load external modules
 require("lazy-bootstrap") -- load lazy bootstrap
-
-require("keymaps") -- load our keymaps file
-
 require("settings") -- load our settings file
-
+require("keymaps") -- load our keymaps file
 require("autocmds") -- load autocmds file
-
 require("lazy").setup("plugins") -- loads everything in /plugins
