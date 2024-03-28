ed763b9a5e86cc04470eea0fa088c8ebf469f406
Erik Haake <ehaake@pdx.edu>
2024-03-28 16:28:01 -0700

    Add a couple more autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -19,9 +19,42 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 vim.api.nvim_create_autocmd("FileType", {
 	group = vim.api.nvim_create_augroup("vertical_help", { clear = true }),
 	pattern = "help",
+	desc = "Open help windows in vertical split",
 	callback = function()
 		vim.bo.bufhidden = "unload"
 		vim.cmd.wincmd("L")
 		vim.cmd.wincmd("=")
 	end,
 })
+
+
+-- From Dillon Mulroy's setup:
+-- https://github.com/dmmulroy/kickstart.nix/blob/main/config/nvim/lua/user/edit_text.lua
+-- 
+-- Turn on spell checking and text wrapping for certain filetypes
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	group = vim.api.nvim_create_augroup("edit_text", { clear = true }),
+	pattern = { "gitcommit", "markdown", "txt" },
+	desc = "Enable spell checking and text wrapping for certain filetypes",
+	callback = function()
+		vim.opt_local.wrap = true
+		vim.opt_local.spell = true
+	end,
+})
+
+-- From Dillon Mulroy's setup:
+-- https://github.com/dmmulroy/kickstart.nix/blob/main/config/nvim/lua/user/toggle_diagnostics.lua
+--
+-- Toggle diagnostics
+vim.api.nvim_create_user_command("ToggleDiagnostics", function()
+	if vim.g.diagnostics_enabled == nil then
+		vim.g.diagnostics_enabled = false
+		vim.diagnostic.disable()
+	elseif vim.g.diagnostics_enabled then
+		vim.g.diagnostics_enabled = false
+		vim.diagnostic.disable()
+	else
+		vim.g.diagnostics_enabled = true
+		vim.diagnostic.enable()
+	end
+end, {})
