5ff1ee586909f899684448af55628b54fd738746
Erik Haake <ehaake@pdx.edu>
2024-03-28 16:36:29 -0700

    Add visual code indent reselect lines

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -52,6 +52,10 @@ vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right win
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
+-- Visual code movment
+vim.keymap.set("v", "<", "< gv")
+vim.keymap.set("v", ">", "> gv")
+
 -- UI Stuff
 --
 -- Toggle 80 width column
