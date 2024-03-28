a3cd02794e6469dc0cd9959cb41e0b962945a067
Erik Haake <ehaake@pdx.edu>
2024-03-28 16:18:26 -0700

    Add help in vsplit autocmd

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -11,3 +11,17 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- From Dillon Mulroy's setup:
+-- https://github.com/dmmulroy/kickstart.nix/blob/main/config/nvim/lua/user/vertical_help.lua
+-- 
+-- Open help windows in vertical split
+vim.api.nvim_create_autocmd("FileType", {
+	group = vim.api.nvim_create_augroup("vertical_help", { clear = true }),
+	pattern = "help",
+	callback = function()
+		vim.bo.bufhidden = "unload"
+		vim.cmd.wincmd("L")
+		vim.cmd.wincmd("=")
+	end,
+})
