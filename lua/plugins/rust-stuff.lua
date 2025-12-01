0579e57c4089c0d4a2c1da8ff9e7ed5718d8cd04
Erik Haake <ehaake@pdx.edu>
2025-11-30 19:44:08 -0800

    Update rustaceanvim stuff for v6

--- lua/plugins/rust-stuff.lua
+++ lua/plugins/rust-stuff.lua
@@ -3,33 +3,30 @@
 return {
 	{
 		"mrcjkb/rustaceanvim",
-		version = "^5", -- Recommended
-		lazy = false,
+		version = "^6", -- Recommended
+		lazy = false,   -- plugin is already lazy internally
 		ft = { "rust" },
 
 		config = function()
-			return {
-				-- Plugin configuration
+			-- LSP config for rust-analyzer via rustaceanvim
+			vim.g.rustaceanvim = {
 				tools = {},
-				-- LSP configuration
 				server = {
-					on_attach = function(client, bufnr)
-						-- you can also put keymaps in here
+					on_attach = function(_, bufnr)
 						vim.keymap.set("n", "<leader>ca", function()
-							vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-							-- or vim.lsp.buf.codeAction() if you don't want grouping.
+							vim.cmd.RustLsp("codeAction")
+						end, { silent = true, buffer = bufnr })
+
+						vim.keymap.set("n", "K", function()
+							vim.cmd.RustLsp({ "hover", "actions" })
 						end, { silent = true, buffer = bufnr })
 					end,
 					default_settings = {
-						-- rust-analyzer language server configuration
 						["rust-analyzer"] = {
-							cargo = {
-								allFeatures = true,
-							},
+							cargo = { allFeatures = true },
 						},
 					},
 				},
-				-- DAP configuration
 				dap = {},
 			}
 		end,
